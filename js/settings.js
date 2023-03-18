02da7713e476debf27aff73510d274dca4b12c10
SchoolIzBoring <123842815+SchoolIzBoring@users.noreply.github.com>
2023-03-17 22:22:52 -0500

    Delete js directory

--- js/settings.js
+++
@@ -1,9 +0,0 @@
-localStorage.setItem('previousPage', window.location.href);
-window.location.href = 'settings.html';
-var previousPage = localStorage.getItem('previousPage');
-if (previousPage) {
-    window.location.href = previousPage;
-} else {
-    // If the previous page is not found, redirect to the homepage or a default page
-    window.location.href = 'index.html';
-}
